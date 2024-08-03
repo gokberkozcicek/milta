ce4157a36d88a0572f1af55bc324c2278c78a09a
gokberkozcicek <gokberkozcicek@gmail.com>
2024-08-03 23:01:29 +0300

    Add section collection view model #10

--- MILTA_GUI/MILTA_GUI/Main.cs
+++ MILTA_GUI/MILTA_GUI/Main.cs
@@ -59,6 +59,7 @@ namespace MILTA_GUI
         }
         private void UpdateTreeView()
         {
+            mainTreeView.Nodes.Clear();
             CustomTreeNode projectNode=new CustomTreeNode(miltaProject);
             foreach (var shaft in miltaProject.ShaftCollection)
             {
@@ -128,6 +129,10 @@ namespace MILTA_GUI
                         break;
                     case MiltaObjectTypes.Section:
                         viewModel = new SectionViewModel();
+                        break;
+                    case MiltaObjectTypes.SectionCollection:
+                        viewModel = new SectionCollectionViewModel();
+                        (viewModel as SectionCollectionViewModel).UpdateTreeView = UpdateTreeView;
                         break;
                     case MiltaObjectTypes.Shaft:
                         viewModel = new ShaftViewModel();
---
+++ MILTA_GUI/MILTA_GUI/ViewModels/SectionCollectionViewModel.cs
@@ -0,0 +1,51 @@
+﻿
+
+using MiltaCore;
+using MiltaCore.Sections;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MILTA_GUI
+{
+    public class SectionCollectionViewModel : ViewModel
+    {
+        private ShaftData shaft;
+        private SectionCollection _sectionCollection { get; set; }
+        private ContextMenuStrip contextMenuStrip;
+        public delegate void UpdateTreeViewDelegate();
+        public UpdateTreeViewDelegate UpdateTreeView;
+
+        public override void InitViewModel()
+        {
+            if (BaseObject != null)
+            {
+                _sectionCollection = BaseObject as SectionCollection;
+                shaft = _sectionCollection.ParentObject.ParentObject as ShaftData;
+                PropertyGrid.SelectedObject = null;
+
+                //context menu
+                contextMenuStrip = new ContextMenuStrip();
+                ToolStripMenuItem addSectionMenuItem = new ToolStripMenuItem("Add Section");
+                contextMenuStrip.Items.Add(addSectionMenuItem);
+                addSectionMenuItem.Click += AddSectionMenuItem_Click;
+                TreeView.ContextMenuStrip = contextMenuStrip;
+            }
+        }
+
+        private void AddSectionMenuItem_Click(object sender, EventArgs e)
+        {
+            SectionData section=_sectionCollection.AddDummyOuterSection();
+            UpdateTreeView?.Invoke();
+
+        }
+
+        
+
+
+    }
+}
